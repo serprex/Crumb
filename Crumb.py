f47c9c44174fcb64490f578bab6619d3882940f5
Demur Rumed <junkmute@hotmail.com>
2010-10-05 20:01:38 +0000

    Silly me was using lines for the scores rather than glRect. Allowed leaving glVertex2f compressing to zlib and reassigning n to glRecti so to save 7 bytes

--- Crumb.py
+++ Crumb.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 #coding=utf-8
 import zlib
-exec zlib.decompress('xÚmSÁnÛ0½÷+Œž¨DöæÚËÎaÅ°KÃëÁ0\n%fT²$È\n’þýH\'K»¡6Jz"õž½~Ì¢r3ÓâfÏXxÕÓGX¡íž¤“^ŽRK#MáÝóÕ±µjÜªÑöÁ¢Š !¶¥ÜÑxnMÂTO#”«ê¾Bø˜§RÛßžîö4}ðÖÇŠg¿p—E¿UvQÂ Ê£¡®ÅÓàËz-iù·8ÕZ¾›q	8]Þ	‰ÿ#ÄhAÛo¨ƒRHe^Jz…tàð”àY,ê|%ë\0m¿»®¤ór„¸\Ë˜SÌ×Z"@2D¤÷”°¬)_SžbNÊPŸ]/Ì>‹Ú	¯MV*µ8ïq`‹àÌç¼MÅž”U™qÙYo‹´Qä÷›M)zâe¶kîzv`–ë|q3;9úÃ„ì§nß­GŸŒw‡3EækK¶’-rK<‡OfH/@R"N˜.ÌÝ!N>‚)4AÓë”pü7s»‹~š^”‰·dÊØÎW\ÕŸ¿²M+	?÷Ö«´¨õØÄŒ(¶ ;šêdþ’Î—ÜTŠÁK—Ø(÷\nü©á§9‘–°€ gN‡ýBn»¼Ì±°ißçH™O´Î+Ê\wˆ®i0/—Ä¹àØÕ}Gr÷¬vxS¼T|º¼ wc`½yÑ°èô€ÚN\`®Çå6 :×ãM|6\'Ð„o{Nd»¥B(âÁøKQ)')
+exec zlib.decompress('xÚmSMoÛ0½÷W=Q‰ìÍq–5œÃ†a—†ÖƒaJL«dI´ý÷#¯Í†Ù(~“ïÙCôc•ëI˜1ø˜V7ÛÂ«¶˜þg+´}3OÒK\'µ4ÒÞ=öQ=7VÇ^ÕÚ~µ¨"hˆM)Ot“0uÓå®ºÛ\n!|Ì.WI	ÞúX\rtû‰§dHú£²ˆâz•P>ô¹x˜|Úï%!ÿt¤NBÃë—€ÝåFHü×B\rx(óRÒ+¤‡/	Åj›ïäF^k4Øy9×{y¢s’ùžMT¤È„l¢¤ë”°Þ’K~’9-Nµ0C3´‚¶_PÕ’JsÍ1ÚþÂ˜ðe3€-‚0|¿ŠT™qÙW‹¬Èï‡RtT »*°²m½éò*ûÍõ ÄÍLÝèÏ2º¹ÒGŸŒw¥™"¶µ%‰y¤÷ÆáƒéÓÌ†\'LKæé\'ÁšLÓë”püÛs{Š~šž”‰·Dˆkæ¥wÛŸ™¢„ï÷ƒõ*­*A3Ö1£[1HCM2:—m•bã2%ÖÊ½Cªÿa^]XAsN‹ÝBnÛ¼Ì±+°-±Ñå,™¤çyÞ"D[×˜—kÊsÜÐµÛ®%Ü;†=¼Ã^*®.—ò4Æ›•šA§Ôqâs?nwXÕ¥ñ"LN ?ßvìÈ&fK…PÄ³ñë“$Ý')
