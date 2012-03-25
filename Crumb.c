1299ee7280dd0b810e733403635d05bf6db7516d
Demur Rumed <gunkmute@gmail.com>
2012-03-25 13:51:12 +0000

    Makefile, shrink Crumb.c, remove GLU dependency

--- Crumb.c
+++ Crumb.c
@@ -1,5 +1,5 @@
 #include <GL/glfw.h>
-#define D;glfwGetMousePos(&a,&b)
+#define D;glfwGetMousePos(&a,&b);
 #define U(x,y)glColor3f(x,y,y);
-#define B(z)glRecti(x-z,y-z,x+z,y+z)
-main(){glfwInit();glfwDisable(196609);glfwOpenWindow(599,599,0,0,0,0,0,0,65537);gluOrtho2D(0,599,599,0);glLineWidth(2);srand(glfwGetTime());i:glClear(16384);int s=0,x,y,a,b,c,d,e,f,g[3];s:U(0,0)D;e=c=a;f=d=b;B(9);while((x=rand()&511)<99||(y=rand()&511)<99);B(9);U(1,0)B(4);U(0,1)glRecti(s*6-4,595,s*6,597);s++;U(1,1)while(!glfwGetKey(257)&&glfwGetWindowParam(131073)){int p=abs(c-a),q=abs(d-b),i=0,j=0;if(p*p+q*q>9&&a>0&&b>0&&a<599&&b<599){while(p>q?(j=d==b?:i*q/p,i++<p):(i=c==a?:j*p/q,j++<q)){glReadPixels((c<a?c:a)+i,599-(d<b?d:b)-j,1,1,6407,5122,g);if(g[1])goto i;if(g[0])goto s;}glBegin(1);glVertex2i(e,f);glVertex2i(e=c,f=d);glEnd();c=a;d=b;}glfwSwapBuffers()D;}}
+#define B(z)glRecti(x-z,y-z,x+z,y+z);
+main(){glfwInit();glfwDisable(196609);glfwOpenWindow(599,599,0,0,0,0,0,0,65537);glOrtho(0,599,599,0,0,1);glLineWidth(2);srand(glfwGetTime());i:glClear(16384);int s=0,x,y,a,b,c,d,e,f,g[3];s:U(0,0)D e=c=a;f=d=b;B(9)while((x=rand()&511)<99||(y=rand()&511)<99);B(9)U(1,0)B(4)U(0,1)glRecti(s*6-4,595,s*6,597);s++;U(1,1)while(!glfwGetKey(257)&&glfwGetWindowParam(131073)){int p=abs(c-a),q=abs(d-b),i=0,j=0;if(p*p+q*q>9&&a>0&&b>0&&a<599&&b<599){while(p>q?(j=d==b?:i*q/p,i++<p):(i=c==a?:j*p/q,j++<q)){glReadPixels((c<a?c:a)+i,599-(d<b?d:b)-j,1,1,6407,5122,g);if(g[1])goto i;if(g[0])goto s;}glBegin(1);glVertex2i(e,f);glVertex2i(e=c,f=d);glEnd();c=a;d=b;}glfwSwapBuffers()D}}
--- Crumb.py
+++ Crumb.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python2
+#!/usr/bin/python
 #coding=latin
 exec "xÚmSÁŽÚ0½óÑžÆàd	¡´DÊZU½ôPõÐ=D2dÈZrlË1þ¾3	lÑª kfžgüÆoœcp}”mÉèÞ»ç³#cþÚŒÿÃ²Î¼Ãád¥“VvRK9»kƒ:WFõûV•ùfPè T¹<ÐÚU:b\0âëòMñe-„p!™\IÎ¸PÉû‡¨Éº½2ˆìä[Qž5µzÎ^GŸ¶[IKÈ;#1uà+¾¶x;_	‰ª¨ÀÁRæ2Ò‚ÅK„˜¯Ó\ÉÇˆÚâ<ús^XlåVHÉ¦[†<…HË„9Èïé~±¦½5í‘Mé²ÔEÝ}LB‚f@èÌWì´…b*ãnÆœÎüÁñ²:‚É¼ó@ÇÝq$ÅT¢m2éhÐ‚Ï«t#ªMjç¦.W\r+<*c¾Û„˜“êÝi@žWW=Ä½‹ÚÙÛEÃíij4frO'üÔ_uß€dÕT9`¼•Na &ÐYGØp\"ö¶žÁ\rÃ›Òá‰F`+øAçÅy!@”!q.KÒ#u	D=>év/…bðÖ–Ê^ßˆjé)	s˜JjlàSS§yŠ˜ƒ©Iø&eCJ<Sœ´óž!½¨Ë’Ü”+y›õò3?zÓë´õ²aÍý?ÍÁIÅ\òvœ<ôžÕæ dÉéj?0ÝÈÎä/7 ˜Ø9‰oÅ£ñôyàÄE8Oq”\Ì”÷Ùèþ|ñ!d".decode('zip')
